5327efff3f51fb0ca14be9c11af80a866e429c8e
saravana-pandian <saravanaoff2000@gmail.com>
2021-06-02 17:02:44 +0530

    Security access rights

--- models/models.py
+++ models/models.py
@@ -1,18 +1,6 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+class Employees(models.Model):
+    _name = 'spoints.employees'
 
-
-# class spoints(models.Model):
-#     _name = 'spoints.spoints'
-#     _description = 'spoints.spoints'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
